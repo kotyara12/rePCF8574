a8491926fa6c79dc2081040b9d927fc3199185e4
kotyara12 <kotyara12@yandex.ru>
2022-03-23 19:50:31 +0300

    First commit

---
+++ include/rePCF8574.h
@@ -0,0 +1,55 @@
+/* 
+   EN: Class for working with the PCF8574 port expander
+   RU: Класс для работы с расширителем портов PCF8574
+   --------------------------------------------------------------------------------
+   (с) 2022 Разживин Александр | Razzhivin Alexander
+   kotyara12@yandex.ru | https://kotyara12.ru | tg: @kotyara1971
+*/
+
+#ifndef __RE_PCF8574_H__
+#define __RE_PCF8574_H__
+
+#include <stdint.h>
+#include <esp_err.h>
+#include "driver/i2c.h"
+#include "driver/gpio.h"
+#include "rTypes.h"
+#include "reGpio.h"
+
+#define PCF8574_READ_ERROR 0x80
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+class rePCF8574 {
+  public:
+    rePCF8574(i2c_port_t numI2C, uint8_t addrI2C, cb_gpio_change_t callback);
+    ~rePCF8574();
+
+    // Reading and writing data from the chip with changing the internal buffer
+    uint8_t get(bool read_i2c = true);
+    bool set(uint8_t data);
+
+    // Reading and writing individual bits (stateful in internal buffer)
+    uint8_t read(uint8_t pin, bool read_i2c = true);
+    bool write(uint8_t pin, const uint8_t value);
+
+    // Reread data from i2c. If changes are detected, events will be posted to the event loop. Returns the bits that have been changed
+    uint8_t update();
+  private:
+    i2c_port_t _numI2C = 0; 
+    uint8_t _addrI2C = 0;
+    uint8_t _data = 0xFF;
+    cb_gpio_change_t _callback = nullptr;
+
+    // Physical reading and writing data from the chip without changing the internal buffer
+    bool read8(uint8_t* data);
+    bool write8(uint8_t data);
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __RE_PCF8574_H__
---
+++ src/rePCF8574.cpp
@@ -0,0 +1,125 @@
+#include "rePCF8574.h"
+#include "reI2C.h"
+#include "rLog.h"
+#include "reEvents.h"
+
+static const char* logTAG = "PCF8574";
+
+#define ERR_CHECK(err, str) if (err != ESP_OK) { \
+  rlog_e(logTAG, "%s: #%d (%s)", str, err, esp_err_to_name(err)); \
+  return false; \
+};
+
+#define PCF8574_TIMEOUT 1000
+
+rePCF8574::rePCF8574(i2c_port_t numI2C, uint8_t addrI2C, cb_gpio_change_t callback)
+{
+  _numI2C = numI2C; 
+  _addrI2C = addrI2C;
+  _callback = callback;
+}
+
+rePCF8574::~rePCF8574()
+{
+}
+
+// -----------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------- I2C ---------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------
+
+bool rePCF8574::read8(uint8_t* data)
+{
+  esp_err_t err = readI2C(_numI2C, _addrI2C, nullptr, 0, data, 1, 0, PCF8574_TIMEOUT); 
+  if (err == ESP_OK) {
+    // rlog_d(logTAG, "Read from PCF8574 %d.0x%2x: 0x%2x", _numI2C, _addrI2C, *data);
+    return true;
+  };
+  rlog_e(logTAG, "Failed to read PCF8574 %d.0x%2x: %d (%s)", _numI2C, _addrI2C, err, esp_err_to_name(err));
+  return false;
+}
+
+bool rePCF8574::write8(uint8_t data)
+{
+  esp_err_t err = writeI2C(_numI2C, _addrI2C, nullptr, 0, &data, 1, PCF8574_TIMEOUT);
+  if (err == ESP_OK) {
+    // rlog_d(logTAG, "Write to PCF8574 %d.0x%2x: 0x%2x", _numI2C, _addrI2C, data);
+    return true;
+  };
+  rlog_e(logTAG, "Failed to write PCF8574 %d.0x%2x: %d (%s)", _numI2C, _addrI2C, err, esp_err_to_name(err));
+  return false;
+}
+
+uint8_t rePCF8574::get(bool read_i2c)
+{
+  if (read_i2c) {
+    read8(&_data);
+  };
+  return _data;
+}
+
+bool rePCF8574::set(uint8_t data)
+{
+  if (write8(data)) {
+    _data = data;
+    return true;
+  };
+  return false;
+}
+
+uint8_t rePCF8574::read(uint8_t pin, bool read_i2c) 
+{
+  if (read_i2c) {
+    if (!read8(&_data)) {
+      return PCF8574_READ_ERROR;
+    };
+  };
+  return (_data & (1 << pin)) > 0;
+}
+
+bool rePCF8574::write(uint8_t pin, const uint8_t value) 
+{
+  if (value == 0) {
+    _data &= ~(1 << pin);
+  } else {
+    _data |= (1 << pin);
+  };
+  return write8(_data);
+}
+
+// -----------------------------------------------------------------------------------------------------------------------
+// -------------------------------------------------- Changes detection --------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------
+
+uint8_t rePCF8574::update()
+{
+  // Read new data from i2c ( reset INT!!! )
+  uint8_t new_data = 0;
+  if (!read8(&new_data)) {
+    return 0;
+  };
+  
+  // Compare previous and new data
+  uint8_t changes = _data ^ new_data;
+  _data = new_data;
+
+  if (changes != 0) {
+    // Prepare data for events
+    gpio_data_t data;
+    data.bus = (uint8_t)_numI2C + 1;
+    data.address = _addrI2C;
+
+    // Send events only for pins that have changed
+    for (uint8_t i = 0; i < 8; i++) {
+      if ((changes & (1 << i)) > 0) {
+        data.pin = i;
+        data.value = (uint8_t)((_data & (1 << i)) > 0);
+        eventLoopPost(RE_GPIO_EVENTS, RE_GPIO_CHANGE, &data, sizeof(data), portMAX_DELAY);
+        if (_callback) {
+          _callback((void*)this, data, 0);
+        };
+      };
+    };
+  };
+
+  return changes;
+}
